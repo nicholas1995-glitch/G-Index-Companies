da7d7d3dfee7df44bdc8f633be03dbebdf28a8a2
nicholas1995-glitch <Nicholas.gazzola@gmail.com>
2025-02-26 08:17:27 +0100

    Aggiornamento xpath price

--- scraper.py
+++ scraper.py
@@ -1,13 +1,5 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-import time
-import logging
-from google.cloud import firestore
-from google.oauth2 import service_account
-from datetime import datetime
+import requests
+from lxml import html
 import random
 import os
 import json
@@ -156,17 +148,20 @@ companies_info = {
     "ATCO-A.ST": {"name": "Atlas Copco A", "isin": "SE0011166610"}
 }
 
-options = Options()
-options.add_argument("--headless")  # Esegui in background senza aprire una finestra
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--window-size=1920x1080")
-options.add_argument("start-maximized")
-options.add_argument("disable-infobars")
-options.add_argument("--disable-extensions")
+# Elenco User-Agent
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+    " Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)"
+    " Chrome/90.0.4430.93 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15"
+    " (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+    # Aggiungi altri User-Agent se necessario...
+]
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
+session = requests.Session()
+session.headers.update({"User-Agent": random.choice(USER_AGENTS)})
 
 def scrape_stock_data(ticker):
     try:
